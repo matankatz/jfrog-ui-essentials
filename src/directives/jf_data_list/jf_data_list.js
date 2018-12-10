3d33724f0ac1b401e650a2d62075331ca1fb9e41
Evgeni Dikerman <evgenid@jfrog.com>
2018-12-10 15:02:27 +0200

    JFUIE-123-Test playground data_list test init

--- src/directives/jf_data_list/jf_data_list.js
+++ src/directives/jf_data_list/jf_data_list.js
@@ -1,33 +1,32 @@
 class jfDataListController {
-	constructor($scope, $rootScope, $element, $timeout, JFrogModal, JFrogUIUtils) {
-		this.$scope = $scope;
-		this.$rootScope = $rootScope;
-		this.$element = $element;
-		this.$timeout = $timeout;
-		this.JFrogModal = JFrogModal;
-		this.JFrogUIUtils = JFrogUIUtils;
+    constructor($scope, $rootScope, $element, $timeout, JFrogModal, JFrogUIUtils) {
+        this.$scope = $scope;
+        this.$rootScope = $rootScope;
+        this.$element = $element;
+        this.$timeout = $timeout;
+        this.JFrogModal = JFrogModal;
+        this.JFrogUIUtils = JFrogUIUtils;
 
-		this.$scope.$watch('jfDataList.items', items => {
-			if (items) {
-				this.formattedItems = _.filter(items, (item) => {
-					return item.label != '';
-				});
-			}
-		})
-	}
+        this.$scope.$watch('jfDataList.items', items => {
+            if (items) {
+                this.formattedItems = _.filter(items, (item) => {
+                    return item.label != '';
+                });
+            }
+        })
+    }
 
-	/**
-	 * htmlIsOverflowing(rowId)
-	 * Get al the row's children.
-	 * Then sum all the children's box model width (includes padding and margin) in a loop
-	 * If the child already has the overflowing class => remove it
-	 * When the sum gets > then the container's width => add the overflowing class to him
-	 * After exiting the loop return the overflowing flag
-	 * **/
-	htmlIsOverflowing(rowId) {
-        // this.$timeout(function() {
-            let elem = this.$element.find(rowId);
-
+    /**
+     * htmlIsOverflowing(rowId)
+     * Get al the row's children.
+     * Then sum all the children's box model width (includes padding and margin) in a loop
+     * If the child already has the overflowing class => remove it
+     * When the sum gets > then the container's width => add the overflowing class to him
+     * After exiting the loop return the overflowing flag
+     * **/
+    htmlIsOverflowing(rowId) {
+        let elem = this.$element.find(rowId);
+        if (elem.length) {
             let children = elem.children('.tag');
             let maxWidth = elem.closest('.data-list-item-value').outerWidth() - 60;
             let totalChildrenWidth = 0;
@@ -46,58 +45,61 @@ class jfDataListController {
 
             });
             return elem.children('.tag.overflowing-child').length > 0;
-        // },0);
-	}
+        }
+        else{
+            return false;
+        }
+    }
 
-	showAll(model, rowName, objectName) {
-		//objectName = _.startCase(objectName.indexOf('/') >= 0 ? objectName.split('/')[0] : this.objectName);
+    showAll(model, rowName, objectName) {
+        //objectName = _.startCase(objectName.indexOf('/') >= 0 ? objectName.split('/')[0] : this.objectName);
 
-		let modalScope = this.$rootScope.$new();
-		modalScope.items = model;
-		modalScope.rowName = rowName;
-		modalScope.objectName = objectName;
+        let modalScope = this.$rootScope.$new();
+        modalScope.items = model;
+        modalScope.rowName = rowName;
+        modalScope.objectName = objectName;
 
-		modalScope.filter = {};
-		modalScope.filterItem = (item) => {
-			if (modalScope.filter.text) {
-				let escaped = this.JFrogUIUtils.escapeForRegex(modalScope.filter.text);
-				let regex = new RegExp('.*' + escaped.split('\\*').join('.*') + '.*', 'i');
-				return regex.test(item.label);
-			}
-			else {
-				return true;
-			}
-		};
+        modalScope.filter = {};
+        modalScope.filterItem = (item) => {
+            if (modalScope.filter.text) {
+                let escaped = this.JFrogUIUtils.escapeForRegex(modalScope.filter.text);
+                let regex = new RegExp('.*' + escaped.split('\\*').join('.*') + '.*', 'i');
+                return regex.test(item.label);
+            }
+            else {
+                return true;
+            }
+        };
 
-		modalScope.noResults = () => {
-			let filteredResults = _.filter(modalScope.items, (item) => {
-				return modalScope.filterItem(item);
-			});
-			return filteredResults.length === 0;
-		};
+        modalScope.noResults = () => {
+            let filteredResults = _.filter(modalScope.items, (item) => {
+                return modalScope.filterItem(item);
+            });
+            return filteredResults.length === 0;
+        };
 
-		this.JFrogModal.launchModalWithTemplateMarkup(require('./jf_data_list.show_all_modal.html'), modalScope, 'sm', true);
-	}
+        this.JFrogModal.launchModalWithTemplateMarkup(require('./jf_data_list.show_all_modal.html'), modalScope, 'sm', true);
+    }
 
-	isArray(o) {
-		return Array.isArray(o);
-	}
+    isArray(o) {
+        return Array.isArray(o);
+    }
 
-	getItemValue(value) {
-		return value || '&nbsp';
-	}
+    getItemValue(value) {
+        return value || '&nbsp';
+    }
 }
 
 
 export function jfDataList() {
-	return {
-		restrict: 'E',
-		scope: {
-			items: '='
-		},
-		controller: jfDataListController,
-		controllerAs: 'jfDataList',
-		bindToController: true,
-		templateUrl: 'directives/jf_data_list/jf_data_list.html'
-	}
+    return {
+        restrict: 'E',
+        scope: {
+            items: '='
+        },
+        controller: jfDataListController,
+        controllerAs: 'jfDataList',
+        bindToController: true,
+        templateUrl: 'directives/jf_data_list/jf_data_list.html'
+    }
 }
